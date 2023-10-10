068095b5dd80420f39fbcafbd2040ac2d5fc9805
G00394595@atu.ie <G00394595@atu.ie>
2023-10-10 16:12:24 +0100

    Start

---
+++ src/main/java/com/example/lab3/Part3/LibaryService.java
@@ -0,0 +1,13 @@
+package com.example.lab3.Part3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibaryService {
+    List<Book> book = new ArrayList<>();
+
+    public void addBook(Book book){
+
+        this.book.add(book);
+    }
+}
