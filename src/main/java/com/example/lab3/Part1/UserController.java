325897ef89863d59b51f2b0adbd6cd9ae29a3811
G00394595@atu.ie <G00394595@atu.ie>
2023-10-10 15:38:28 +0100

    Start

--- src/main/java/com/example/lab3/Part1/UserController.java
+++ src/main/java/com/example/lab3/Part1/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class UserController {
 UserService userService;
+
   @GetMapping("/registerUser/{name}/{email}")
         public String Register(@PathVariable String name, @PathVariable String email){
 
